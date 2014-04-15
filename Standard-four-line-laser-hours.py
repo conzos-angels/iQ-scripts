0cba9bf0c1b25de8282053fafae9be4e47af271a
Francissss <f.brady@andor.com>
2014-04-15 11:17:56 -0400

    Update Standard-four-line-laser-hours.py
    Check laser hours on a Coherent 405, 488, Cobolt 561 and Coherent 640 ALC setup

--- Standard-four-line-laser-hours.py
+++ Standard-four-line-laser-hours.py
@@ -1,8 +1,9 @@
-# Program to get laser hours from a standard 405,488,561(not Gen3),640 ALC set-up
+# Program to get laser hours from a standard 405,488,561 Gen3 and lower,640 ALC set-up   15-Apr-14
 
+# Getting Coherent laser hours
 def coherent():
 	# Initialise COM Port
-	port=serial.Serial(comport,baud, timeout=3)
+	port=serial.Serial(comport,baud,timeout=3)
 	# Write to laser
 	port.write('?HH\r'.encode('latin-1'))
 	# Wait
@@ -12,17 +13,36 @@ def coherent():
 	# Close COM Port
 	port.close()
 	
+# Getting Cobolt laser hours
 def cobolt():
 	# Initialise COM port
-	port=serial.Serial(comport,baud, timeout=3)
+	port=serial.Serial(comport,baud,timeout=3)
+	# Write to laser
 	port.write('hrs?\r'.encode('latin-1'))
 	# Wait
-	time.sleep(1)	
-	# Print the laser hours	
-	print(comport, ':   ' + port.read(10).decode('latin-1'))
-	# Close COM Port
-	port.close()
-	
+	time.sleep(1)
+	# Check if data has been received, if not try Gen3 baud rate
+	buffer = port.read(port.inWaiting()).decode('latin-1')
+	if '\n' in buffer:
+		# Print the laser hours	
+		print(comport, ':   ' + port.read(10).decode('latin-1'))
+		# Close COM Port
+		port.close()
+		
+	else:
+		# Close COM Port
+		port.close()
+		# Initialise COM port
+		port=serial.Serial(comport,115200,timeout=3)
+		# Write to laser
+		port.write('hrs?\r'.encode('latin-1'))
+		# Wait
+		time.sleep(1)
+		# Print the laser hours	
+		print(comport, ':   ' + port.read(10).decode('latin-1'))
+		# Close COM Port
+		port.close()
+		
 import time
 import serial
 # Set Baud
